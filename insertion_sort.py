8146596eba5a01fd0ffb60bfc1f9f9c9a1abf6e1
Ricky McEvoy <rickymcevoy9@gmail.com>
2025-03-10 12:23:32 -0500

    Update insertion_sort.py
    changed insertion sort had the wrong code for it

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,33 +1,17 @@
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
+def insertionSort(arr):
+    n = len(arr) 
+     
+    if n <= 1:
+        return 
 
-        merge_sort(left)
-        merge_sort(right)
-
-        i = j = k = 0
-
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[k] = left[i]
-                i += 1
-            else:
-                arr[k] = right[j]
-                j += 1
-            k += 1
-
-        while i < len(left):
-            arr[k] = left[i]
-            i += 1
-            k += 1
-
-        while j < len(right):
-            arr[k] = right[j]
-            j += 1
-            k += 1
-
-arr = [38, 27, 43, 3, 9, 82, 10]
-merge_sort(arr)
+    for i in range(1, n): 
+        key = arr[i] 
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j] 
+            j -= 1
+        arr[j+1] = key 
+ 
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
 print(arr)
